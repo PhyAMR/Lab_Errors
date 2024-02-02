6cdd4c27f2b8a431a31e41b79ed4230e9747c630
Alvaro Mendez <alvaromendezrt@gmail.com>
2024-02-02 09:10:03 +0100

    Started creating the app with flet

---
+++ App.py
@@ -0,0 +1,76 @@
+import functions as fn
+import flet as ft
+from flet.matplotlib_chart import MatplotlibChart
+
+
+def main(page: ft.Page):
+    page.title = "Calculadora de errores de laboratorio"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    page.appbar = ft.AppBar(
+        title=ft.Text(
+            "Calculadora de errores de laboratorio", weight=ft.FontWeight.BOLD, color=ft.colors.BLACK87
+        ),
+        bgcolor=ft.colors.BLUE,
+        center_title=True,
+        actions=[
+            ft.IconButton(ft.icons.MENU, tooltip="Menu",
+                          icon_color=ft.colors.BLACK87)
+        ],
+        color=ft.colors.WHITE,
+    )
+
+    def btn_click(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please enter the formula"
+            page.update()
+        else:
+            name = txt_name.value
+            a, b = fn.calculate_error(name, ['g'])
+            fig = fn.render_formula(b)
+            page.add(MatplotlibChart(fig, expand=True, scale=0.5))
+            page.update()
+
+    txt_name = ft.TextField(label="Your formula")
+
+    page.add(txt_name)
+
+    C = ft.ElevatedButton("Calculate", on_click=btn_click)
+
+    dlg = ft.AlertDialog(
+        title=ft.Text(""" Usage \t To use this calculator enter your latex formula, click evaluate to add the value of the constants 
+                      \t and then click calculate """), on_dismiss=lambda e: print("Dialog dismissed!")
+    )
+
+    def close_dlg(e):
+        dlg.open = False
+        page.update()
+
+    def open_dlg(e):
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
+    I = ft.ElevatedButton("Instructions", on_click=open_dlg)
+
+    def btn_click2(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please enter the formula"
+            page.update()
+        else:
+            name = txt_name.value
+            vars = fn.detect_var(name)
+            for i in vars:
+                fig = fn.render_formula(i)
+                page.add(MatplotlibChart(fig, expand=True, scale=0.5))
+
+            page.update()
+    E = ft.ElevatedButton("Evaluate", on_click=btn_click2)
+    r = ft.Row([
+        ft.Container(expand=1, content=C),
+        ft.Container(expand=1, content=E),
+        ft.Container(expand=1, content=I)
+    ])
+    page.add(r)
+
+
+ft.app(target=main)
