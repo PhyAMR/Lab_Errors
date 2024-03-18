7ef3648edd8116be964b37b871b231359e613797
Álvaro Méndez Rodríguez de Tembleque <118479554+PhyAMR@users.noreply.github.com>
2024-03-18 21:48:54 +0100

    Eliminated the simplification method for checking

--- functions.py
+++ functions.py
@@ -77,6 +77,7 @@ def calculate_error(formula, const):
         before = ' '
         expression = formula
 
+    #error = syp.simplify(Error(expression, const))
     error = Error(expression, const)
     error_formula = before + str(syp.latex(error))
     error_show = str(error_formula.replace('\Delta', '\Delta '))
